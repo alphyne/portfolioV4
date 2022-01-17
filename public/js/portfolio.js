8e2eb8e1bf4ce77d808017330138835c122c2b4c
unknown <suasin.ja@gmail.com>
2022-01-16 18:27:08 -0800

    Add floating top button, update hero images

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -16,62 +16,46 @@ $(function() {
   });
 });
 
-$(document).ready(function (){
 
+
+
+
+
+// Set a variable for our button element.
+const scrollToTopButton = document.getElementById('js_top');
+
+// Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
+const scrollFunc = () => {
+  // Get the current scroll value
+  let y = window.scrollY;
   
-  $('#nav-work').hover(function(){
-      var navLabel = $('#nav-work-content').html();
-      navLabel = 'UNDER CONSTRUCTION';
-      $('#nav-work-content').html(navLabel);
-  });
-
-  $('#nav-work').mouseleave(function(){
-    var navLabel = $('#nav-work-content').html();
-    navLabel = 'WORK';
-    $('#nav-work-content').html(navLabel);
-  });
-
-  $("#work").click(function (){
-      $('html, body').animate({
-          scrollTop: $("#navbar").offset().top,
-          behavior: 'smooth'
-      }, 800);
-  });
-
-  $("#prototype").click(function (){
-    $('html, body').animate({
-        scrollTop: $("#embed").offset().top,
-        behavior: 'smooth'
-    }, 800);
-});
-});
-
-var animateHTML = function() {
-  var elems;
-  var windowHeight;
-  function init() {
-    elems = document.querySelectorAll('.hidden');
-    windowHeight = window.innerHeight;
-    addEventHandlers();
-    checkPosition();
+  // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+  if (y > 0) {
+    scrollToTopButton.className = "button_top show";
+  } else {
+    scrollToTopButton.className = "button_top hide";
   }
-  function addEventHandlers() {
-    window.addEventListener('scroll', checkPosition);
-    window.addEventListener('resize', init);
-  }
-  function checkPosition() {
-    for (var i = 0; i < elems.length; i++) {
-      var positionFromTop = elems[i].getBoundingClientRect().top;
-      if (positionFromTop - windowHeight <= 0) {
-        elems[i].className = elems[i].className.replace(
-          'hidden',
-          'fade-in'
-        );
-      }
-    }
-  }
-  return {
-    init: init
-  };
 };
-animateHTML().init();
+
+window.addEventListener("scroll", scrollFunc);
+
+const scrollToTop = () => {
+  // Let's set a variable for the number of pixels we are from the top of the document.
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+  
+  // If that number is greater than 0, we'll scroll back to 0, or the top of the document.
+  // We'll also animate that scroll with requestAnimationFrame:
+  // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    // ScrollTo takes an x and a y coordinate.
+    // Increase the '10' value to get a smoother/slower scroll!
+    window.scrollTo(0, c - c / 10);
+  }
+};
+
+// When the button is clicked, run our ScrolltoTop function above!
+scrollToTopButton.onclick = function(e) {
+  e.preventDefault();
+  scrollToTop();
+}
